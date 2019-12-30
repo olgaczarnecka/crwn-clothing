6cf95bf31ec2413e134ed9d06e385c93c74ebf6d
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2019-12-30 17:15:54 +0100

    update shop data with new data, update cart utils to group item in cart reducer

--- src/App.js
+++ src/App.js
@@ -44,7 +44,6 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route
-            exact
             path="/signin"
             render={() =>
               this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
